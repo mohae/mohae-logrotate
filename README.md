1dab7c31e642c1b5241a29a04a0319accd89e38e
Count Derp <thecount@moehae.com>
2013-08-18 19:26:04 -0500

    initial setup of wrapper

---
+++ README.md
@@ -0,0 +1,13 @@
+# mohae-logrotate cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: mohae-logrotate
+# Recipe:: default
+#
+# Copyright (C) 2013 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+include_recipe 'logrotate'
